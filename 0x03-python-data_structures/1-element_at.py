6004c1e2aaa22ab2a444dd628d874abc485e89c9
Nayocean <Goodyearrotiba>
2023-01-07 14:58:41 -0800

    drip drip

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-def elemennt_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return ("None")
+def element_at(my_list, idx):
+    my_list_len = len(my_list)
+    if idx < 0 or idx > my_list_len - 1:
+        return 'None'
     else:
         return my_list[idx]
