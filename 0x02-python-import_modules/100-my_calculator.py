dbba8e52da0759e8c22a567c0e5bf9a70ed5c652
Nayocean <Goodyearrotiba>
2023-01-07 14:08:42 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+    import sys
 
-    a = 10
-    b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
-    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
-    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
-    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
+    args_len = len(sys.argv)
+    if args_len != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+        a = int(sys.argv[1])
+        opr = sys.argv[2]
+        b = int(sys.argv[3])
+
+        if opr == '+':
+            print("{} {} {} = {}".format(a, opr, b, add(a, b)))
+        elif opr == '-':
+            print("{} {} {} = {}".format(a, opr, b, sub(a, b)))
+        elif opr == '*':
+            print("{} {} {} = {}".format(a, opr, b, mul(a, b)))
+        elif opr == '/':
+            print("{} {} {} = {}".format(a, opr, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
