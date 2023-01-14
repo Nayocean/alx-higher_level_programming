d2dd487ab94dd9b1355ccb08ebbab63e7f832e8f
Nayocean <Goodyearrotiba>
2023-01-13 22:37:40 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     def find_search(element):
         return element if element != search else replace
-return list(map(find_search, my_list))
+    return list(map(find_search, my_list))
